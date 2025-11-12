6738d6dc60b5ef1c914205d893519642e6cf7ef3
Renan Malveira Rodrigues <renan.pocket@gmail.com>
2025-11-12 15:13:53 -0300

    Update - Application Quit in the MainMenu

--- Tatics Fruits/Assets/Scripts/AndroidQuitHandler.cs
+++ Tatics Fruits/Assets/Scripts/AndroidQuitHandler.cs
@@ -1,5 +1,7 @@
-using System;
 using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
 
 public class AndroidQuitHandler : MonoBehaviour
 {
@@ -16,16 +18,36 @@ public class AndroidQuitHandler : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        
     }
 
+#if UNITY_ANDROID && !UNITY_EDITOR
     private void Update()
     {
-#if UNITY_EDITOR
+        bool backPressed = false;
+
+        // Sistema antigo (Input Manager)
         if (Input.GetKeyDown(KeyCode.Escape))
+            backPressed = true;
+
+        // Sistema novo (Input System)
+        #if ENABLE_INPUT_SYSTEM
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+            backPressed = true;
+        #endif
+
+        if (backPressed)
         {
-            Debug.Log("Botar voltar pressionando - Encerrando o jogo");
+            Debug.Log("Back button pressionado - encerrando o jogo");
             Application.Quit();
+
+            // Se quiser **minimizar** em vez de fechar:
+            // using (var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+            // using (var activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+            // {
+            //     activity.Call<bool>("moveTaskToBack", true);
+            // }
         }
-#endif
     }
+#endif
 }
