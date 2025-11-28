1a0bd31ce657b594e505989d4a5de2b0c8c6ccea
Renan Malveira Rodrigues <renan.pocket@gmail.com>
2025-11-28 10:48:41 -0300

    Revert "Feature - Mobile responsive"
    This reverts commit 9ec6ee697c5450bed9161c795760488873818898.

--- Tatics Fruits/Assets/Scripts/Gameplay/Utils/SafeAreaAdjuster.cs
+++ Tatics Fruits/Assets/Scripts/Gameplay/Utils/SafeAreaAdjuster.cs
@@ -1,104 +1,100 @@
+using System;
 using UnityEngine;
 
-namespace Gameplay.Utils
+public class SafeAreaAdjuster : MonoBehaviour
 {
-    public class SafeAreaAdjuster : MonoBehaviour
+    [Tooltip("Aplique somente no eixo X/Y")]
+    public bool applyX = true;
+    public bool applyY = true;
+    
+    [Tooltip("Margens suaves extras (px): Left, Top, Right, Bottom")]
+    public Vector4 softMargins = Vector4.zero;
+
+    private Rect _lastSafe;
+    private Vector2 _lastScreen;
+    private RectTransform _rectTransform;
+
+    private void Awake()
     {
-        [Tooltip("Apply only on X/Y axis")]
-        public bool applyX = true;
-        public bool applyY = true;
-        
-        [Tooltip("Extra soft margins (px): Left, Top, Right, Bottom")]
-        public Vector4 softMargins = Vector4.zero;
+        _rectTransform = GetComponent<RectTransform>();
+        Apply();
+    }
 
-        private Rect lastSafe;
-        private Vector2 lastScreen;
-        private RectTransform rectTransform;
+    private void Start()
+    {
+        Apply();
+    }
 
-        private void Awake()
-        {
-            rectTransform = GetComponent<RectTransform>();
-            Apply();
-        }
-
-        private void Start()
-        {
-            Apply();
-        }
-
-        private void OnEnable()
-        {
-            Apply();
-        }
-        
+    private void OnEnable()
+    {
+        Apply();
+    }
+    
 #if UNITY_EDITOR
-        private void Update()
-        {
-            ApplyIfChanged();
-        }
+    private void Update()
+    {
+        ApplyIfChanged();
+    }
 #endif
 
-        private void OnRectTransformDimensionsChange()
+    private void OnRectTransformDimensionsChange()
+    {
+        ApplyIfChanged();
+    }
+
+    private void ApplyIfChanged()
+    {
+        if (_lastSafe != Screen.safeArea || _lastScreen.x != Screen.width || _lastScreen.y != Screen.height)
         {
-            ApplyIfChanged();
+            Apply();
+        }
+    }
+
+    public void Apply()
+    {
+        if(_rectTransform == null)
+                _rectTransform = GetComponent<RectTransform>();
+        
+        var safe = Screen.safeArea;
+        _lastSafe = safe;
+        _lastScreen = new Vector2(Screen.width, Screen.height);
+        
+        var min = safe.position;
+        var max = safe.position + safe.size;
+        min.x /= Screen.width;
+        max.x /= Screen.width;
+        min.y /= Screen.height;
+        max.y /= Screen.height;
+
+        if (!applyX)
+        {
+            min.x = 0f;
+            max.x = 1f;
         }
 
-        private void ApplyIfChanged()
+        if (!applyY)
         {
-            if (lastSafe != Screen.safeArea || lastScreen.x != Screen.width || lastScreen.y != Screen.height)
-            {
-                Apply();
-            }
+            min.y = 0f;
+            max.y = 1f;
         }
+        
+        _rectTransform.anchorMin = min;
+        _rectTransform.anchorMax = max;
+        
+        _rectTransform.offsetMin = Vector2.zero;
+        _rectTransform.offsetMax = Vector2.zero;
 
-        public void Apply()
+        if (softMargins != Vector4.zero)
         {
-            if(rectTransform == null)
-                rectTransform = GetComponent<RectTransform>();
-            
-            var safe = Screen.safeArea;
-            lastSafe = safe;
-            lastScreen = new Vector2(Screen.width, Screen.height);
-            
-            var min = safe.position;
-            var max = safe.position + safe.size;
-            min.x /= Screen.width;
-            max.x /= Screen.width;
-            min.y /= Screen.height;
-            max.y /= Screen.height;
-
-            if (!applyX)
-            {
-                min.x = 0f;
-                max.x = 1f;
-            }
-
-            if (!applyY)
-            {
-                min.y = 0f;
-                max.y = 1f;
-            }
-            
-            rectTransform.anchorMin = min;
-            rectTransform.anchorMax = max;
-            
-            rectTransform.offsetMin = Vector2.zero;
-            rectTransform.offsetMax = Vector2.zero;
-
-            if (softMargins != Vector4.zero)
-            {
-                float w = Screen.width, h = Screen.height;
-                var m = rectTransform.anchorMin;
-                var M = rectTransform.anchorMax;
-                m.x += softMargins.x / w;
-                M.x -= softMargins.z / w;
-                M.y -= softMargins.y / h;
-                m.y += softMargins.w / h;
-                rectTransform.anchorMin = m;
-                rectTransform.anchorMax = M;
-            }
-            
-            Debug.Log($"[SafeArea] Applied safe area: {safe}, Screen: {Screen.width}x{Screen.height}");
+            float w = Screen.width, h = Screen.height;
+            var m = _rectTransform.anchorMin;
+            var M = _rectTransform.anchorMax;
+            m.x += softMargins.x / w;
+            M.x -= softMargins.z / w;
+            M.y -= softMargins.y / h;
+            m.y += softMargins.w / h;
+            _rectTransform.anchorMin = m;
+            _rectTransform.anchorMax = M;
         }
     }
 }
